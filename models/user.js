7c0b03f1cd04faa323374e7591a01d8a056d08fc
Marvellous Ediagbonya <marvellousediagbonya683@gmail.com>
2024-12-14 16:44:36 +0100

    commit

--- models/user.js
+++ models/user.js
@@ -12,19 +12,26 @@ email: {
 },
 
 number: {
-  type:String,
+  type: Number,
+  min:1,
+  max:12,
   required:true
 },
 
-selectoption: {
+select: {
   type:String,
+  enum: ['option1', 'option2', 'option3', 'option4', 'option5'], // Predefined set of values
   required:true
+
 },
 textarea: {
   type:String,
-  required:true
-},
+  required:true,
+  minlength: 10, // Minimum length
+    maxlength: 500
+}
 });
-
 const User = mongoose.model('user', userSchema);
+console.log(`USER MODEL LOADED`)
+
 module.exports = User;
